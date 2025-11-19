3cb8953eb49b6c379dc7f84eb86505af239d32ed
Mohamed Abdelhady <mohamedabdelhady.official@gmail.com>
2025-11-20 01:00:08 +0200

    Initial commit

---
+++ server/src/test/java/com/university/management/system/demo/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.management.system.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
